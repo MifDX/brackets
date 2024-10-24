56ea5683156a41b5f7127d9d38196e55752282f2
MifDX <104031797+MifDX@users.noreply.github.com>
2024-10-24 20:17:41 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const parentheses = '()';
+  const braces = '{}';
+  const squareBrackets = '[]';
+  const straightBrackets = '||';
+  
+while (
+  str.includes(parentheses) ||
+  str.includes(braces) ||
+  str.includes(squareBrackets) ||
+  str.includes(straightBrackets)) 
+  {str = str.replaceAll(parentheses, '')
+  .replaceAll(braces, '')
+  .replaceAll(squareBrackets, '')
+  .replaceAll(straightBrackets, '')
 }
+return !str;
+};
